ad745a5295775432a02634b6a3e1c113745cf3a2
yousuf777555 <98573971+yousuf777555@users.noreply.github.com>
2022-04-27 16:57:19 +0530

    Create extern.c

---
+++ extern.c
@@ -0,0 +1,27 @@
+//file1
+
+#include<stdio.h>
+int main()
+{
+     register int a=5;
+     printf("%d\n",a);
+      
+}
+  //file2
+#include<stdio.h>
+int main()
+{
+     register int a=5;
+     printf("%d\n",a);
+    
+     
+}
+     
+
+    
+
+
+
+     
+
+    
